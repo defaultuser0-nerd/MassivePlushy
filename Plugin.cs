a4e96c4818b49e761b9e778f3f014915929585ed
defaultuser0 <axolotlslmao@gmail.com>
2024-12-13 20:59:54 -0600

    plushymod

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+namespace sigmamodtemplate
+{
+    [BepInPlugin("defaultuser0.plushymod", "MassivePlushy", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        void Start()
+        {
+            GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+        }
+
+        void OnGameInitialized()
+        {
+            GameObject plushy = GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/MonkePlushyDisplay/MonkePlushy_Prefab Variant/MonkePlushy/");
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/MonkePlushyDisplay/table (3)/").SetActive(false);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/MonkePlushyDisplay/MonkePlushy_Prefab Variant/Glass_Base/").SetActive(false);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/MonkePlushyDisplay/MonkePlushy_Prefab Variant/Monke_Base/").SetActive(false);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/MonkePlushyDisplay/").SetActive(true);
+            plushy.transform.localScale = new Vector3(5f, 5f, 5f);
+            plushy.transform.position = new Vector3(-63.5583f, 11.7282f, -82.86f);
+            plushy.transform.localRotation = Quaternion.Euler(7.5f, 167.5869f, 0f);
+        }
+    }
+}
