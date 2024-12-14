3bc4c7c4ddacc72bc6c0e3fb500e95a6d04ba589
defaultuser0 <axolotlslmao@gmail.com>
2024-12-14 08:03:41 -0600

    Readd PluginInfo.cs because I found it easier

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace MassivePlushy
+{
+    class PluginInfo
+    {
+        public const string GUID = "defaultuser0.massiveplushy";
+        public const string Name = "MassivePlushy";
+        public const string Version = "1.1.0";
+    }
+}
