a56a364dcd056b018367db06655a604866c76d49
defaultuser0 <axolotlslmao@gmail.com>
2024-12-14 12:20:16 -0600

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using System;
 using UnityEngine;
 
@@ -23,6 +23,9 @@ namespace MassivePlushy
             plushy.transform.localScale = new Vector3(5f, 5f, 5f);
             plushy.transform.position = new Vector3(-63.5583f, 11.7282f, -82.86f);
             plushy.transform.localRotation = Quaternion.Euler(7.5f, 167.5869f, 0f);
+            GameObject plushy2 = GameObject.Find("Environment Objects/LocalObjects_Prefab/City_WorkingPrefab/MonkePlushy (combined by EdMeshCombiner)/");
+            plushy2.transform.localScale = new Vector3(5f, 5f, 5f);
+            plushy2.transform.position = new Vector3(-44.4318f, 13.4633f, 16.0535f);
         }
     }
 }
